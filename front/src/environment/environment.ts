44ce7ef6a248ab9af4b3206a4ba19639136c4492
med <mohammedbouajaja1@gmail.com>
2024-07-22 17:33:00 +0200

    add other file side front

---
+++ front/src/environment/environment.ts
@@ -0,0 +1,10 @@
+
+
+
+export const environment = {
+    production: false,
+    backendHost: 'http://localhost:8085',
+    jsonUrlProducts:  '../../../../assets/products.json',
+    isReadDataFromJsonFile: false,
+
+}
